96b08ce9d19ef34d8bae9660b42eaa36c9c2a216
Humphrey Obuobi <obuobhum@gmail.com>
2016-05-27 00:37:11 -0500

    progress on the portfolio page, added images and basic layout

---
+++ js/portfolio-data.js
@@ -0,0 +1,13 @@
+var portfolio_data =
+{
+    'shindo': {
+      'path': 'img/shindo002.jpg',
+      'title': 'SHINDO',
+      'description': 'Test stuff i guess'
+    },
+    'bud': {
+      'path': 'img/BUD002.jpg',
+      'title': 'BUD',
+      'description': 'More test stuff for Bud, i guess'
+    }
+}
