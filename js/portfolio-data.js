803143844f6c031b4dda4e725c90328f6a314bb4
hobuobi <obuobhum@gmail.com>
2016-05-27 20:33:14 -0500

    changed the capitalization on the filenames

--- js/portfolio-data.js
+++ js/portfolio-data.js
@@ -11,17 +11,17 @@ var portfolio_data =
       'description': 'BUD was built to be an aesthetically appealing desktop companion that responds to the conditions of the user and environment; through pulling data from the Microsoft Band and the Weather Channel API, the petals of the wooden flower would open or close according to this information. The project was built with students from Brown University and Rhode Island School of Design at Hack@Brown 2016. Due to time constraints, the project could not be completed at the event, but all of the physical components were designed and built on site.'
     },
     'dh': {
-        'path': 'img/DH.jpg', 
+        'path': 'img/DH.JPG', 
         'title': 'MONITOR HOLDER', 
         'description': 'A personal project, this project was built to be a self-balancing, multiple angle stand for a portable monitor (ASUS MB168B+). The angle can be adjusted by simply pushing or pulling the frame of the stand. The most recent iteration of the project includes a 3D-printed base to more thoroughly secure the angle of the screen.'
     },
     'cs171': {
-        'path': 'img/CS171_001.jpg', 
+        'path': 'img/CS171_001.JPG', 
         'title': 'CS171 PROJECT', 
         'description': "In collaboration with Kevin Zhang and Patrick Pan. In our final project for the Harvard University class CS171, Data Visualzation, our team extracted data on an individual's use of Facebook Messenger and built a series of visualizations of said data (including message length, message volume, and ratio of sent/received messages over time, as well as a visualization of summary statistics). The project was built for both web and desktop using the Electron framework."
     },
     'gcode': {
-        'path': 'img/meander001.jpg', 
+        'path': 'img/meander001.JPG', 
         'title': 'CUSTOM GCODE', 
         'description': "At the Wyss Institute for Biologically Inspired Engineering, I worked with the Lewis Group Bioprinting Team to develop custom MATLAB scripts to generate GCODE for a variety of purposes, including printing of biological materials, silicone gaskets, and multi-layer structures of hydrogels. Additionally, I derived a series of algorithms to extend the typical capabilities of GCODE, including a more intuitive way to generate arcs of various starting and ending angles and heights."
     },
